bc9a406645737981b5443ec92cdf4394ec727d57
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-09-30 12:28:17 +0300

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+
+    # GET /show
+    def show
+        user = User.find(params[:id])
+        render json: user, status: :ok, include: :items
+    end
+
+
 end
